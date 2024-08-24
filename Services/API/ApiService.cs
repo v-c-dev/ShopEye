284b11d2fdda9e07ba5c18a9d255f87b5ffc78cf
Victor-dev <144972612+v-c-dev@users.noreply.github.com>
2024-08-24 15:22:38 -0300

    feat: ApiService and DatabaseService implemented

---
+++ Services/API/ApiService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using ShopEye.Models;
+
+namespace ShopEye.Services.API
+{
+    public class ApiService : IApiService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public ApiService(string apiKey)
+        {
+            _httpClient = new HttpClient();
+            _apiKey = apiKey;
+        }
+
+        public async Task<Item> GetItemDetailsAsync(string barcode)
+        {
+            var url = $"https://api.barcodespider.com/v1/lookup?token={_apiKey}&barcode={barcode}";
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Item>(content);
+        }
+    }
+}
