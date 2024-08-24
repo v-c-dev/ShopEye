343c0cb4649bdb1cb46ecaeab3e92e118abcc1ef
Victor-dev <144972612+v-c-dev@users.noreply.github.com>
2024-08-24 13:57:42 -0300

    Initial Commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopEye
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ShopEye;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
