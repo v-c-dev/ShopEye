06495230c939b4afa782d887df452846bee55b72
Victor-dev <144972612+v-c-dev@users.noreply.github.com>
2024-08-24 15:12:24 -0300

    feat: entities and repository implemented

---
+++ Models/Entities/ItemEntity.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+
+namespace ShopEye.Models.Entities
+{
+    public class ItemEntity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Name { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Description { get; set; }
+
+        [NotNull]
+        public DateTime Scandate { get; set; } = DateTime.Now;
+
+        public string UPC { get; set; }
+    }
+}
