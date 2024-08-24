528ec82d9c7cd6fc42c45d6f54bb0fbdbeab8488
TheV <vitoralcanfor@hotmail.com>
2024-08-24 15:43:59 -0300

    appicon, splashscreen, androidmanifest n program files implemented

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace ShopEye
+﻿using ShopEye.Views;
+
+namespace ShopEye
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(ScanPage), typeof(ScanPage));
+            Routing.RegisterRoute(nameof(ScanHistoryPage), typeof(ScanHistoryPage));
+            Routing.RegisterRoute(nameof(AboutPage), typeof(AboutPage));
+            Routing.RegisterRoute(nameof(MoreInfoPage), typeof(MoreInfoPage));
         }
     }
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ShopEye.Services.API;
+using ShopEye.Services.Database;
+using ZXing.Net.Maui;
+using ZXing.Net.Maui.Controls;
 
 namespace ShopEye
 {
@@ -13,11 +18,35 @@ namespace ShopEye
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .UseBarcodeReader()
+
+            #region ZXing-bug-prevention
+                .ConfigureMauiHandlers(h =>
+                {
+                    h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraBarcodeReaderView), typeof(CameraBarcodeReaderViewHandler));
+                    h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraView), typeof(CameraViewHandler));
+                    h.AddHandler(typeof(ZXing.Net.Maui.Controls.BarcodeGeneratorView), typeof(BarcodeGeneratorViewHandler));
                 });
+            #endregion
+
+            #region database
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "mydatabase.db3");
+            builder.Services.AddSingleton<IDatabaseService>(s => new DatabaseService(dbPath));
+            #endregion
+
+            //TODO: Use Secure storage instead of appsettings
+            #region barcodelookup
+            builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            string apiKey = builder.Configuration["ApiSettings:BarcodeSpiderApiKey"];
+            builder.Services.AddSingleton<IApiService>(s => new ApiService(apiKey));
+            #endregion
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
+
+
 
             return builder.Build();
         }
